7d04ac3c338d540f1d85cc9dd97205fd60d383d9
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-06-29 22:15:02 +0200

    Update Main.java

--- EnigmaMachine/src/Main.java
+++ EnigmaMachine/src/Main.java
@@ -1,15 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        while(true){
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+                String letter = reader.readLine();
+                System.out.print("input a letter to encode: ");
+                System.out.println();
+                if(letter.equals("")){
+                    break;
+                }
+                else{
+                    letter = letter.toUpperCase();
+                    //letter = encodeLetter(letter, plugboard, rotors, reflector); //encode letter
+                    //rotor = updateRotors(rotors, rotorsRotation); // update rotor position
+                    System.out.println("encoded letter: " + letter + "\n");
+                }
+            }catch (IOException e){
+                System.out.println("something went wrong");
+            }
+
         }
     }
 }
