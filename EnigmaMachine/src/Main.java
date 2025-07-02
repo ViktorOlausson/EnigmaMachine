ae6a0b019cdd37be50a14c316fd50468fe874e76
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-07-02 11:00:15 +0200

    Update Main.java

--- EnigmaMachine/src/Main.java
+++ EnigmaMachine/src/Main.java
@@ -1,20 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static final char[] ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ".toCharArray();
+
+    static Map<Character, Character> plugboard(int nrPairs){
+        Map<Character, Character> plugboard = new HashMap<>();
+        List<Integer> freeIndexes = new ArrayList<>();
+
+        for(int i = 0; i < ALPHABET.length; i++){
+            freeIndexes.add(i);
+        }
+
+        char[] finalCombinations = new char[ALPHABET.length];
+        for
+
+        return plugboard;
+    }
     public static void main(String[] args) {
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // to see how IntelliJ IDEA suggests fixing it
+
         while(true){
 
             try{
+                System.out.print("input a letter to encode: ");
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
                 String letter = reader.readLine();
-                System.out.print("input a letter to encode: ");
                 System.out.println();
                 if(letter.equals("")){
                     break;
