7907e35e85439f8db4fbafb8737d8f98654fa6e4
Viktor Olausson <121167588+ViktorOlausson@users.noreply.github.com>
2025-07-19 14:08:25 +0200

    added numbers
    added numbers, out commented becuase real enigma machine does not include numbers
    left to do:
    move stuff to separete files.
    Fix so reflector uses POSSIBLE_REFLECTOR_COMBINATIONS or make createRotors work the same as createReflector
    Add "?" key

--- EnigmaWithGui/src/main/java/com/enigmamachine/enigmawithgui/EnigmaController.java
+++ EnigmaWithGui/src/main/java/com/enigmamachine/enigmawithgui/EnigmaController.java
@@ -230,6 +230,7 @@ public class EnigmaController {
 
     public void initialize(){
         String[][] keyRows = {
+                //{"1","2","3","4","5","6","7","8","9","0","?"},
                 {"Q","W","E","R","T","Y","U","I","O","P","Å"},
                 {"A","S","D","F","G","H","J","K","L","Ö","Ä"},
                 {"Z","X","C","V","B","N","M",",","+",".","-"},
